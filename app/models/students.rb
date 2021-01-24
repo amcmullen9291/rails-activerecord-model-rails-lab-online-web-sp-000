623432c0321ef22758dffb554c0caee930fab2ea
anthony mcmullen <amcmullen9291@gmail.com>
2021-01-24 00:41:12 +0000

    Automatically backed up by Learn

--- app/models/students.rb
+++ app/models/students.rb
@@ -1,2 +1,6 @@
-class Students < ActiveRecord::RUBY_RELEASE_DATE
+class Students < ActiveRecord::Base 
+  
+  def to_s (first_name, last_name)
+    @name = (first_name + " " + last_name)
+    @name
 end
