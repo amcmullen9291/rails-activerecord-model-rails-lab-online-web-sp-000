2ac65ff1930083b7ae5d3031d626ecbd6d0bc988
anthony mcmullen <amcmullen9291@gmail.com>
2021-01-24 00:48:05 +0000

    Automatically backed up by Learn

--- app/models/students.rb
+++ app/models/students.rb
@@ -1,7 +1,7 @@
 class Students < ActiveRecord::Base 
   
   def to_s (first_name, last_name)
-    @name = (first_name + " " + last_name)
+    @name = (first_name + " " + last_name).to_s
     @name
   end
 end
